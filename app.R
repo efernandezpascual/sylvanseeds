6fd4bc69df053c9e45abf32e2bea752badbedd03
efernandezpascual <61799380+efernandezpascual@users.noreply.github.com>
2020-03-17 14:31:19 +0100

    Initial commit

---
+++ app.R
@@ -0,0 +1,56 @@
+library(shiny)
+
+library(tidyverse); library(maps)
+source("visualizeDB.R")
+load("data/WoSDB.RData")
+spp <- WoSDB %>% select(Species) %>% arrange(Species) %>% pull(Species) %>% unique %>% as.character
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Sylvan Seeds: Germination database of temperate broadleaf and mixed forests"),
+
+    # Sidebar to choose species to display
+    sidebarLayout(
+         sidebarPanel(helpText("Select a species to display germination records."),
+            selectInput("var",
+                        label = "Species:",
+                        choices = spp,
+                        selected = spp[1])
+        ),
+
+        # Show a plot of the germination records
+        mainPanel(
+           plotOutput("plot_map"),
+           plotOutput("plot_germination"),
+           tableOutput("table")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$plot_map <- renderPlot({
+        # filter by species
+        x  <- filtrar(Places, input$var)
+        
+        # draw the map for the species
+        mapplot(df = x, shape = TBMF)
+    })
+    
+    output$plot_germination <- renderPlot({
+        # filter by species
+        y  <- filtrar(Germination, input$var)
+        
+        # draw the germination plot for the species
+        seedplot(df = y)
+    })
+    output$table <- renderTable({
+        references(WoSDB, References, input$var)
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
